c82d0ead35ba1d24728fdbf655d623a495bea51d
Welder Victor <weldersalvadorvictor12@hotmail.com>
2023-08-06 21:59:17 -0300

    projeto em andamento

---
+++ aula 12ex/ex 14/script.js
@@ -0,0 +1,21 @@
+
+function carregar(){
+    var data = new Date()
+    //var hora = data.getHours()
+    var fundo = document.getElementById('fundo')
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('img')
+    var hora = 11
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora < 12){
+        img.src = 'manhã.jpg'
+        fundo.style.background = '#edf7d2'
+    } else if (hora >= 12 && hora <= 18){
+        img.src = 'tarde.jpg'
+        fundo.style.background = '#c4915f'
+    } else {
+        img.src = 'noite.jpg'
+        fundo.style.background = '#001118'
+    }
+    
+}
